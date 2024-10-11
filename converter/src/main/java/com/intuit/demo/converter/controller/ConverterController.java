c6b60f52efeb890fdafacacc95f802c47bdb1561
Pruthvi Muddapuram <pruthviraj.muddapuram@nordstrom.com>
2024-10-11 09:03:21 -0400

    Change_log: Added comments and javadoc

--- converter/src/main/java/com/intuit/demo/converter/controller/ConverterController.java
+++ converter/src/main/java/com/intuit/demo/converter/controller/ConverterController.java
@@ -16,9 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(Constants.API)
 public class ConverterController {
   
-  /* Adding request params to future-proof the API  * */
+  /** API to convert one format of data to another format.
+   * As part of the initial release we will support
+   * Markdown to HTML*/
   @PostMapping(Constants.CONVERT)
   public String convertMarkdownToHtml(
+//      Adding request params to future-proof the API
       @RequestParam String from,
       @RequestParam String to,
       @RequestBody String markdown) {
