d2a940cdb66fa64eddc9b9560b61fed904f33467
Pruthvi Muddapuram <pruthviraj.muddapuram@nordstrom.com>
2024-10-11 10:55:13 -0400

    Change_log: Improve code readability by modularization of the code

--- converter/src/main/java/com/intuit/demo/converter/service/impl/ConverterServiceImpl.java
+++ converter/src/main/java/com/intuit/demo/converter/service/impl/ConverterServiceImpl.java
@@ -31,15 +31,6 @@ public class ConverterServiceImpl implements ConverterService {
     return "";
   }
   
-  private String convertHeading(String line) {
-    int headingLevel = 0;
-    while (line.charAt(headingLevel) == '#') {
-      headingLevel++;
-    }
-    String content = line.substring(headingLevel).trim();
-    return String.format("<h%d>%s</h%d>", headingLevel, content, headingLevel);
-  }
-  
   private String convertMarkdownToHtml(String markdown) {
     try {
       StringBuilder html = new StringBuilder();
@@ -50,42 +41,58 @@ public class ConverterServiceImpl implements ConverterService {
         if (line.isEmpty()) {
           continue;
         }
-//        Check if the line is a heading
-        if (line.matches("^#{1,6}.*")) {
+        if (isHeading(line)) {
           html.append(convertHeading(line));
         } else {
-//          Check if the line is unformatted and with a link
-          StringBuilder lineHtml = new StringBuilder();
-          int index = 0;
-          while (index < line.length()) {
-            if (line.charAt(index) == '[') {
-              int endText = line.indexOf(']', index);
-              int startLink = line.indexOf('(', endText);
-              int endLink = line.indexOf(')', startLink);
-              
-              if (endText > 0 && startLink > endText && endLink > startLink) {
-                String linkText = line.substring(index + 1, endText);
-                String link = line.substring(startLink + 1, endLink);
-                
-                lineHtml.append("<a href=\"").append(link).append("\">").append(linkText).append("</a>");
-                index = endLink + 1;
-              } else {
-                lineHtml.append(line.charAt(index));
-                index++;
-              }
-            } else {
-              lineHtml.append(line.charAt(index));
-              index++;
-            }
-          }
-          // Wrap the processed line in a paragraph tag
-          html.append("<p>").append(lineHtml).append("</p>");
+          html.append(convertLineWithLinks(line));
         }
       }
+      
       return html.toString();
     } catch (Exception e) {
       return "Error converting markdown to html";
     }
   }
   
+  private boolean isHeading(String line) {
+    return line.matches("^#{1,6}.*");
+  }
+  
+  private String convertHeading(String line) {
+    int headingLevel = 0;
+    while (line.charAt(headingLevel) == '#') {
+      headingLevel++;
+    }
+    String content = line.substring(headingLevel).trim();
+    return String.format("<h%d>%s</h%d>", headingLevel, content, headingLevel);
+  }
+  
+  private String convertLineWithLinks(String line) {
+    StringBuilder lineHtml = new StringBuilder();
+    int index = 0;
+    
+    while (index < line.length()) {
+      if (line.charAt(index) == '[') {
+        int endText = line.indexOf(']', index);
+        int startLink = line.indexOf('(', endText);
+        int endLink = line.indexOf(')', startLink);
+        
+        if (endText > 0 && startLink > endText && endLink > startLink) {
+          String linkText = line.substring(index + 1, endText);
+          String link = line.substring(startLink + 1, endLink);
+          
+          lineHtml.append("<a href=\"").append(link).append("\">").append(linkText).append("</a>");
+          index = endLink + 1;
+        } else {
+          lineHtml.append(line.charAt(index));
+          index++;
+        }
+      } else {
+        lineHtml.append(line.charAt(index));
+        index++;
+      }
+    }
+//    Wrap the line in a paragraph tag
+    return "<p>" + lineHtml + "</p>";
+  }
 }
